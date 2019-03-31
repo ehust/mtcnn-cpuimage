76ccd8074b6757eace3ef21cd6cc24225e66954c
ehust@hotmail.com <ehust@hotmail.com>
2019-03-31 21:35:53 +0800

    增加build目录，可以直接编译

---
+++ build-hisiv500-linux/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-4.15.0-46-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "4.15.0-46-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "i686")
+
+include("/work/github.com/mtcnn-cpuimage/ncnn/toolchains/hisiv500.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Linux")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ build-hisiv500-linux/CMakeFiles/mtcnn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mtcnn.dir/src/main.cpp.o"
+  "CMakeFiles/mtcnn.dir/src/mtcnn.cpp.o"
+  "mtcnn.pdb"
+  "mtcnn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mtcnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
